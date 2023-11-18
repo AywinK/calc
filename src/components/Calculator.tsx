aa9bc61642e08cff58cf34c619fc0d97af5b3183
Aywin <aywink@hotmail.co.uk>
2023-11-18 14:52:14 +0000

    adds calculator functionality

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,20 +1,26 @@
 import "./Calculator.css";
 import Display from "./Display";
 import ButtonContainer from "./ButtonContainer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as math from "mathjs";
+
+type HistoryObject = {
+  input: string;
+  result: number;
+};
 
 export default function Calculator() {
   const [input, setInput] = useState("");
   const [result, setResult] = useState(0);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState<HistoryObject[]>([]);
+  const [isCleared, setIsCleared] = useState(false);
 
-  const handleClick = (e: unknown): void => {
+  const handleClick = (e: any): void => {
     console.log(e.target.value);
     const isClear = e.target.value === "Clear";
     const isToBeEvaluated = e.target.value === "=";
     if (isClear) {
-      setInput("");
+      setIsCleared(true);
     } else if (isToBeEvaluated) {
       setResult((prev) => {
         if (input) return math.evaluate(input);
@@ -24,6 +30,22 @@ export default function Calculator() {
       setInput((prevState) => prevState + e.target.value);
     }
   };
+
+  useEffect(() => {
+    if (isCleared && input !== "" && result !== 0) {
+      setHistory((prev: HistoryObject[]): HistoryObject[] => [
+        ...prev,
+        { input: input, result: result },
+      ]);
+      setInput("");
+      setResult(0);
+    } else {
+      setInput("");
+      setResult(0);
+    }
+    setIsCleared(false);
+    console.log(history);
+  }, [isCleared]);
 
   return (
     <div className="calculator">
