e0b194e355734620ed74a64edd8a535ab10825f3
Aywin <aywink@hotmail.co.uk>
2023-11-18 12:29:47 +0000

    adds initial states

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,11 +1,15 @@
 import "./Calculator.css";
 import Display from "./Display";
 import ButtonContainer from "./ButtonContainer";
+import { useState } from "react";
 
 export default function Calculator() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
+  const [history, setHistory] = useState([]);
   return (
     <div className="calculator">
-      <Display input={"132+213"} result={342} />
+      <Display input={input} result={result} />
       <ButtonContainer />
     </div>
   );
